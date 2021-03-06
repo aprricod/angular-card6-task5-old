0a899d171469defc202e833b67dbeefa08e70f75
Artem <aprricod@gmail.com>
2021-03-06 20:50:50 +0300

    changes

---
+++ src/app/mod1/mod1.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Mod1Component } from './mod1.component';
+
+@NgModule({
+  declarations: [Mod1Component],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  exports: [Mod1Component],
+})
+export class Mod1Module {}
