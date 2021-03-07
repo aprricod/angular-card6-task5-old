5f7da375a01a6fafda36d9d6d3847d01a8a9a314
Artem <aprricod@gmail.com>
2021-03-07 13:54:34 +0300

    chenges

--- src/app/mod1/mod1.component.ts
+++ src/app/mod1/mod1.component.ts
@@ -1,3 +1,4 @@
+import { formatCurrency } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
@@ -7,20 +8,34 @@ import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./mod1.component.css'],
 })
 export class Mod1Component implements OnInit {
+  toSave;
+  getData;
   form: FormGroup;
   constructor(public fb: FormBuilder) {
     this.form = fb.group({
-      num: fb.control(null),
-      date: fb.control(null),
-      theme: fb.control(null),
-      homework: fb.control(null),
-      note: fb.control(null),
+      list: fb.array([
+        fb.group({
+          num: fb.control(null),
+          date: fb.control(null),
+          theme: fb.control(null),
+          homework: fb.control(null),
+          note: fb.control(null),
+        }),
+      ]),
     });
   }
 
-  // add() {
-  //   (this.form.get('phones') as FormArray).push(this.fb.control(null));
-  // }
+  saveToStorage() {
+    this.toSave = localStorage.setItem('key', 'value');
+  }
+
+  getFromStorage() {
+    this.getData = localStorage.getItem('value');
+  }
+
+  add() {
+    (this.form.get('phones') as FormArray).push(this.fb.control(null));
+  }
 
   ngOnInit(): void {}
 }
