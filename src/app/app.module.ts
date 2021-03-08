6e1a0a761921463e768fb9bf8931b3d4f2a50e85
Artem <aprricod@gmail.com>
2021-03-08 23:11:28 +0300

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { Mod1Module } from './mod1/mod1.module';
+import { Test1Component } from './test1/test1.component';
+import { Test2Component } from './test2/test2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Test1Component,
+    Test2Component
   ],
   imports: [
     BrowserModule,
