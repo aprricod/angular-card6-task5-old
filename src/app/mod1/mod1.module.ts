9cfa41bca4076614891899ecd163acf1f3135172
Artem <aprricod@gmail.com>
2021-03-10 13:23:03 +0300

    fixed error

--- src/app/mod1/mod1.module.ts
+++ src/app/mod1/mod1.module.ts
@@ -3,10 +3,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Mod1Component } from './mod1.component';
+import { Mod1Service } from './mod1.service';
 
 @NgModule({
   declarations: [Mod1Component],
   imports: [CommonModule, FormsModule, ReactiveFormsModule, HttpClientModule],
+  providers: [Mod1Service],
   exports: [Mod1Component],
 })
 export class Mod1Module {}
